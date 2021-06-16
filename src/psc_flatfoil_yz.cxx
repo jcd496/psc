69c7bec4f1bb6c2809f6b15089783bc8705fd804
Kai Germaschewski <kai.germaschewski@unh.edu>
2021-06-16 15:25:16 -0400

    384

--- src/psc_flatfoil_yz.cxx
+++ src/psc_flatfoil_yz.cxx
@@ -287,9 +287,9 @@ Grid_t* setupGrid()
 {
   // --- setup domain
 #if CASE == CASE_3D
-  Grid_t::Real3 LL = {800., 800., 3. * 800.};   // domain size (in d_e)
-  Int3 gdims = {2 * 768, 2 * 768, 3 * 2 * 768}; // global number of grid points
-  Int3 np = {2 * 24, 2 * 24, 3 * 2 * 24};       // division into patches
+  Grid_t::Real3 LL = {800., 800., 3. * 800.}; // domain size (in d_e)
+  Int3 gdims = {384, 384, 3 * 384};           // global number of grid points
+  Int3 np = {12, 12, 3 * 12};                 // division into patches
 #elif CASE == CASE_2D
   Grid_t::Real3 LL = {1., 400., 3. * 400.}; // domain size (in d_e)
   Int3 gdims = {1, 800, 3 * 800};           // global number of grid points
