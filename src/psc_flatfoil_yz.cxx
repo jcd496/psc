4828c5162177f5726de23db482225a7426c1308e
Kai Germaschewski <kai.germaschewski@unh.edu>
2021-06-16 21:04:08 -0400

    1.5x in x

--- src/psc_flatfoil_yz.cxx
+++ src/psc_flatfoil_yz.cxx
@@ -287,9 +287,9 @@ Grid_t* setupGrid()
 {
   // --- setup domain
 #if CASE == CASE_3D
-  Grid_t::Real3 LL = {800., 800., 3. * 800.}; // domain size (in d_e)
-  Int3 gdims = {384, 384, 3 * 384};           // global number of grid points
-  Int3 np = {12, 12, 3 * 12};                 // division into patches
+  Grid_t::Real3 LL = {1.5 * 800., 800., 3. * 800.}; // domain size (in d_e)
+  Int3 gdims = {3 * 384 / 2, 384, 3 * 384}; // global number of grid points
+  Int3 np = {3 * 12 / 2, 12, 3 * 12};       // division into patches
 #elif CASE == CASE_2D
   Grid_t::Real3 LL = {1., 400., 3. * 400.}; // domain size (in d_e)
   Int3 gdims = {1, 800, 3 * 800};           // global number of grid points
@@ -488,7 +488,7 @@ void run()
   checks_params.gauss_every_step = 100;
   checks_params.gauss_dump_always = false;
 #endif
-  checks_params.gauss_threshold = 1e-4;
+  checks_params.gauss_threshold = 1e-3;
   checks_params.gauss_verbose = true;
 
   Checks checks{grid, MPI_COMM_WORLD, checks_params};
